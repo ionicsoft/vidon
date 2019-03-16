18371cbca45b965219c147c61a99b670db85c7f1
Sam <srharrison@cpp.edu>
2019-03-16 19:50:43 +0000

    added search bar logic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,13 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+  
+  def search
+    if params[:search].blank?  
+      redirect_to(shows_path) 
+    else
+      @parameter = params[:search].downcase  
+      @results = Video.all.where("lower(title) LIKE :search", search: "%#{@parameter}%")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get     '/about',   to: 'static_pages#about'
   get     '/contact', to: 'static_pages#contact'
   get     '/signup',  to: 'customers#new'
+  get     '/search',  to: 'static_pages#search', :as => 'search_page'
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
