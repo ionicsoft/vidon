909016ca85d7b774f8d1b6b4d35a8c110969e8ea
jscanning <jerskann@gmail.com>
2019-03-20 03:31:39 +0000

    Merged with 'add-storage' branch. Added ability to upload video files and watch them.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,8 @@
 class Video < ApplicationRecord
   belongs_to :content, polymorphic: true
   has_many :video_comments
-  has_one_attached :video_file
+  has_one_attached :clip
+  has_one_attached :thumbnail
 
   validates :title, presence: true
   validates :description, presence: true
