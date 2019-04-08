c27bdf5051225f273a0b4e1fb77fa97d7c51fcd6
Brandon <bagastelo@cpp.edu>
2019-04-08 00:26:35 +0000

    Start developing unit and integration tests

--- app/models/profile_comment.rb
+++ app/models/profile_comment.rb
@@ -1,5 +1,5 @@
 class ProfileComment < ApplicationRecord
   belongs_to :customer
-  belongs_to :customer, :foreign_key => 'commentor_id'
+  belongs_to :commentor, class_name: "Customer", :foreign_key => 'commentor_id'
   validates :comment, presence: true
 end
---
+++ test/controllers/profile_comments_controller_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class ProfileCommentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @profile_comment = profile_comments(:one)
+  end
+
+  test "should get index" do
+    get profile_comments_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_profile_comment_url
+    assert_response :success
+  end
+
+  test "should create profile_comment" do
+    assert_difference('ProfileComment.count') do
+      get customer_url(@profile_comment.customer)
+      post profile_comments_url, params: { profile_comment: { comment: @profile_comment.comment, commentor_id: @profile_comment.commentor_id, customer_id: @profile_comment.customer_id } }, headers: { 'HTTP_REFERER' => customer_url(@profile_comment.customer) }
+    end
+
+    assert_redirected_to customer_url(@profile_comment.customer)
+  end
+
+  test "should show profile_comment" do
+    get profile_comment_url(@profile_comment)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_profile_comment_url(@profile_comment)
+    assert_response :success
+  end
+
+  test "should update profile_comment" do
+    get customer_url(@profile_comment.customer)
+    patch profile_comment_url(@profile_comment), params: { profile_comment: { comment: @profile_comment.comment, commentor_id: @profile_comment.commentor_id, customer_id: @profile_comment.customer_id } }
+    assert_redirected_to profile_comment_url(@profile_comment)
+  end
+
+  test "should destroy profile_comment" do
+    assert_difference('ProfileComment.count', -1) do
+      delete profile_comment_url(@profile_comment)
+    end
+
+    assert_redirected_to profile_comments_url
+  end
+end
