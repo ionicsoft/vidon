11bd7b9272a88ca540be9b8ef351a55436d962e0
Sam <srharrison@cpp.edu>
2019-04-09 06:12:32 +0000

    Corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,6 @@ Rails.application.routes.draw do
   get     '/search',  to: 'static_pages#search', :as => 'search_page'
   get     '/myshows', to: 'static_pages#shows',  :as => 'my_shows'
   get     '/pro_shows', to: 'static_pages#pro_shows'
-  get     '/newshow', to: 'shows#new'
-  get     '/newmovie', to: 'movies#new'
   get     '/login',   to: 'sessions#new'
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
