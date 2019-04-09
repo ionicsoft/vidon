f7f77bc71bbb376ff86bd758af5455335fdf082a
Brandon <bagastelo@cpp.edu>
2019-04-09 20:10:16 +0000

    Add video comments and style video page

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
   belongs_to :content, polymorphic: true
-  has_many :video_comments
+  has_many :video_comments, dependent: :destroy
   has_one_attached :clip
   has_one_attached :thumbnail
 
