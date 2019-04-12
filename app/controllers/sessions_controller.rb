4536d3dd50260f7c340bb06c01644f148a465959
Brandon <bagastelo@cpp.edu>
2019-04-12 23:39:03 +0000

    Setup persistant session using cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
+  # GET /login
   def new
   end
   
+  # POST /login
   def create
     person = Person.find_by(username: params[:session][:username].downcase)
     if person && person.authenticate(params[:session][:password])
       log_in(person)
+      params[:session][:remember_me] == '1' ? remember(person) : forget(person)
       redirect_to root_url
     else
       flash.now[:danger] = "No user found with username/password combination."
@@ -13,8 +16,9 @@ class SessionsController < ApplicationController
     end
   end
   
+  # DELETE /logout
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,45 @@
 module SessionsHelper
+  
+  # Logs in the given person
   def log_in(person)
     session[:person_id] = person.id
   end
   
+  # Remembers the given person in a persistant session
+  def remember(person)
+    person.remember
+    cookies.permanent.signed[:person_id] = person.id
+    cookies.permanent[:remember_token] = person.remember_token
+  end
+  
+  # Returns the logged in person using token cookie
   def current_person
-    if session[:person_id]
-      @current_person ||= Person.find_by(id: session[:person_id])
+    if (person_id = session[:person_id])
+      @current_person ||= Person.find_by(id: person_id)
+    elsif (person_id = cookies.signed[:person_id])
+      person = Person.find_by(id: person_id)
+      if person && person.authenticated?(cookies[:remember_token])
+        log_in person
+        @current_person = person
+      end
     end
   end
   
+  # Returns true if a person is logged in
   def logged_in?
     !current_person.nil?
   end
   
+  # Forget persistant session
+  def forget(person)
+    person.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  # Logs out the current person
   def log_out
+    forget(current_person)
     session.delete(:person_id)
     @current_person = nil
   end
