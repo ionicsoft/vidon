8c87d746256d8a531ed16c4e843d795cc378db62
Brandon <bagastelo@cpp.edu>
2019-04-16 21:00:00 +0000

    Layout login, customer signup, friends page

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,28 +1,40 @@
 class Person < ApplicationRecord
-    before_save do
-        self.username = username.downcase
-        self.email = email.downcase
-    end
+  before_save do
+    self.username = username.downcase
+    self.email = email.downcase
+  end
 
-    belongs_to :user, polymorphic: true, dependent: :destroy
-    has_one_attached :avatar
-    has_secure_password
-    
-    validates :username, presence: true, length: { minimum: 3 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-    validates :first_name, presence: { if: -> { user.is_a?(Customer) } }
-    validates :last_name, presence: { if: -> { user.is_a?(Customer) } }
+  belongs_to :user, polymorphic: true, dependent: :destroy
+  has_one_attached :avatar
+  has_secure_password
+  
+  validates :username, presence: true, length: { minimum: 3 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :first_name, presence: { if: -> { user.is_a?(Customer) } }
+  validates :last_name, presence: { if: -> { user.is_a?(Customer) } }
 
-    def full_name
-       "#{first_name} #{last_name}"
-    end
+  # Returns the full name of the person as: First Last
+  def full_name
+     "#{first_name} #{last_name}"
+  end
 
-    def customer?
-        user_type == "Customer"
-    end
+  # Returns true if the person is a customer
+  def customer?
+    user_type == "Customer"
+  end
 
-    def producer?
-        user_type == "Producer"
+  # Returns true if person is a producer
+  def producer?
+    user_type == "Producer"
+  end
+  
+  # Returns an link for the avatar if attached, or a placeholder
+  def get_avatar
+    if avatar.attached?
+      url_for avatar
+    else
+      "default_profile.jpg"
     end
+  end
 end
