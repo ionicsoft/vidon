f1cd1184a6ef4a39e9f21d0ec6e41c019af7cafb
Brandon <bagastelo@cpp.edu>
2019-04-17 00:11:15 +0000

    Continue designing pages

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,10 @@
 module PeopleHelper
+  # Returns an link for the avatar if attached, or a placeholder
+  def get_avatar(person)
+    if person.avatar.attached?
+      url_for(person.avatar)
+    else
+      "default_profile.jpg"
+    end
+  end
 end
