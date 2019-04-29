0f34eb8748fead22fc05ba4097228e6052cb7f47
Sam <srharrison@cpp.edu>
2019-04-29 03:13:20 +0000

    Add friends tests

--- test/controllers/friends_controller_test.rb
+++ test/controllers/friends_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class FriendsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @friend = friendships(:one)
+    Capybara.register_driver :selenium do |app|
+        Capybara::Selenium::Driver.new(app, :browser => :firefox)
+    end
+    log_in_as_customer
+  end
+  
+  test "should destroy friendship" do
+    assert_difference('FriendRequest.count', -1) do
+        #not sure how to remove friend here
+        delete friend_url(@friend)
+    end 
+  end
 end
