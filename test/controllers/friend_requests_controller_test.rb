9bfd8f9b41acbee50d4e7e2f0d6276d07155a5db
Brandon <bagastelo@cpp.edu>
2019-05-01 19:00:38 +0000

    Fix friend request bug

--- test/controllers/friend_requests_controller_test.rb
+++ test/controllers/friend_requests_controller_test.rb
@@ -25,4 +25,12 @@ class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
             delete friend_request_url(@req)
         end
     end
+    
+    test "should accept pending request" do
+        post friend_requests_url, params: { friend_request: {customer_id: @customer.id, requester_id: @customer2.id }}
+        log_in_as @customer2.person
+        assert_difference ->{ FriendRequest.count } => -1, ->{ @customer.friends.count } => 1 do
+            post friend_requests_url, params: { friend_request: {customer_id: @customer2.id, requester_id: @customer.id }}
+        end
+    end
 end
